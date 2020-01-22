cfb3964513e4296d48b1335fdb9f3dd2abf5cc07
EugenAchtzehn <eugene1826@gmail.com>
2020-01-22 14:53:21 +0800

    enable the feature of deleting records

--- javascript/all.js
+++ javascript/all.js
@@ -11,7 +11,6 @@ var bmi;
 var clickTime;
 var bmiLevel;
 var borderColor;
-var allRecords;
 
 // 預先定義全域變數
 
@@ -61,15 +60,16 @@ function rateBMI() {
 }
 
 function storeInLocal() {
-  allRecords = localStorage.getItem("historyRecords");
+  let allRecords = localStorage.getItem("historyRecords");
 
   if (allRecords === null) {
     allRecords = [];
     // 若沒有歷史資料，則讓 allRecords 成為空陣列
   } else {
     allRecords = JSON.parse(allRecords);
-    // 若有歷史資料，則開始解析 local storage 資料，轉成陣列
+    // 若有歷史資料，則解析字串，轉成陣列
   }
+  // allRecords 現在已是陣列
 
   let currentRecords = {
     borderColorKey: borderColor,
@@ -81,22 +81,27 @@ function storeInLocal() {
     clickTimeKey: clickTime
   };
 
-  allRecords.push(currentRecords);
-  // 將當前輸入值丟入紀錄陣列尾端
+  allRecords.splice(0, 0, currentRecords);
+  // 將當前輸入值 (currentRecords)，丟入紀錄陣列 (allRecords) 第一位
 
-  allRecords = JSON.stringify(allRecords);
-  // 再將 allRecords 轉成字串丟入 local storage
-
-  localStorage.setItem("historyRecords", allRecords);
-  // 在 local storage 的 key 是 historyRecords
+  localStorage.setItem("historyRecords", JSON.stringify(allRecords));
+  // allRecords 轉回字串丟進 local storage，對應的 key 是 historyRecords
 }
 
 function displayResult() {
-  let allRecordsArray = JSON.parse(allRecords);
-  let recordsLength = allRecordsArray.length;
-  // 用來跑迴圈
-  console.log(allRecordsArray[0].borderColorKey);
-  console.log(recordsLength);
+  let allRecords = localStorage.getItem("historyRecords");
+
+  if (allRecords === null) {
+    allRecords = [];
+    // 若沒有歷史資料，則讓 allRecords 成為空陣列
+  } else {
+    allRecords = JSON.parse(allRecords);
+    // 若有歷史資料，則解析字串，轉成陣列
+  }
+
+  let recordsLength = allRecords.length;
+  // 抓陣列長度來跑迴圈
+
   let totalContent = "";
   // 空字串，用來儲存最後塞入 innerHTML 的內容
 
@@ -104,37 +109,33 @@ function displayResult() {
     totalContent =
       totalContent +
       `
-    <div class="resultRow d-flex" style="border-left: solid 5px` +
-      allRecordsArray[i].borderColorKey +
-      `;">
-    <div class="result-element">` +
-      allRecordsArray[i].bmiLevelKey +
-      `</div>
-    <div class="result-element"><small class="text-sm">BMI: </small><large class="text-lg">` +
-      allRecordsArray[i].bmiKey +
-      `</large></div>
-    <div class="result-element"><small class="text-sm">體重: </small><large class="text-lg">` +
-      allRecordsArray[i].weightKey +
+    <div class="resultRow d-flex" style="border-left: solid 5px ${allRecords[i].borderColorKey};">
+      <div class="result-element">${allRecords[i].bmiLevelKey}</div>
+      <div class="result-element"><small class="text-sm">BMI: </small><large class="text-lg">${allRecords[i].bmiKey}</large></div>
+      <div class="result-element"><small class="text-sm">體重: </small><large class="text-lg">` +
+      allRecords[i].weightKey +
       `</large><small class="text-sm"> kg</small></div>
-    <div class="result-element"><small class="text-sm">身高: </small><large class="text-lg">` +
-      allRecordsArray[i].heightKey +
+      <div class="result-element"><small class="text-sm">身高: </small><large class="text-lg">` +
+      allRecords[i].heightKey +
       `</large><small class="text-sm"> cm</small></div>
-    <div class="result-element">` +
-      allRecordsArray[i].clickTimeKey +
+      <div class="result-element">` +
+      allRecords[i].clickTimeKey +
       `</div>
-    <div class="result-element"><a href="#" class="deleteRecord">delete</a></div>
+      <div class="result-element"><span href="#" data-index="${i}" class="deleteRecord">delete</span></div>
     </div>
     `;
-    console.log(totalContent);
   }
 
-  console.log(totalContent);
   resultDisplay.innerHTML = totalContent;
 }
 
+displayResult();
+// 如果有歷史紀錄，則一載入會就顯示
+
 checkResult.addEventListener(
   "click",
-  function() {
+  function(e) {
+    e.preventDefault;
     manHeightValue = parseFloat(manHeight.value);
     manWeightValue = parseFloat(manWeight.value);
     // 取出輸入數值，並解析成浮點數
@@ -157,9 +158,27 @@ checkResult.addEventListener(
     // 判斷 BMI 對應的健康程度，並先設定好 border 的色碼
 
     storeInLocal();
+    // 取出 local storage 的輸入歷史，並將本次輸入資料存入 local storage
+
     displayResult();
+    // 將全部的資料丟入迴圈，用 innerHTML 顯示到畫面上
   },
   false
 );
 
-// deleteRecord.addEventListener("click", function() {}, false);
+resultDisplay.addEventListener(
+  "click",
+  function(e) {
+    e.preventDefault;
+    if (e.target.tagName !== "SPAN") {
+      return;
+    }
+    // 只有點到 <a>delete</a> 才觸發刪除紀錄功能
+    let index = e.target.dataset.index;
+    let allRecords = JSON.parse(localStorage.getItem("historyRecords"));
+    allRecords.splice(index, 1);
+    localStorage.setItem("historyRecords", JSON.stringify(allRecords));
+    displayResult();
+  },
+  false
+);
